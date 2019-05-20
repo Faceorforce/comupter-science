1b7038fd3cb036d4f1e00d5b23fe192d62a1b709
Your Name <1808248575@qq.com>
2019-05-20 22:22:14 +0800

    19.5.20学习

---
+++ 高级/p01.py
@@ -0,0 +1,14 @@
+
+# 包含一个学生类
+#一个sayhello函数
+#一个打印语句
+
+class  Student():
+    def __init__(self,name="noname",age=18):
+        self.name = name
+        self.age = age
+
+    def say(self):
+        print("my name is {0}",format(self.name))
+
+
